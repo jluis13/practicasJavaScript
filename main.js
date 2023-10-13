2871ac0ea721f8adeda0a292e10ba671ad13933a
Jluis Juarez <j.luis.juarez.r@gmail.com>
2023-10-12 21:51:01 -0600

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("hola koders")
+let a = 10
+let b = 5
+
+let result1 = a + b
+
+let result2 = a - b
+ 
+let result3  = a * b
+
+let result4 = a / b
+
+let result5 = a % b
+
+console.log (result1)
+console.log (result2)
+console.log (result3)
+console.log (result4)
+console.log (result5)
